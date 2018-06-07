9f51237c64581b8b125f894772cea69de26213a3
moxDroid <pritam.world@gmail.com>
2018-06-07 19:15:05 -0400

    New classes added

---
+++ C0738115_MADS3464_MidTerm/src/main/java/com/moxdroidlabs/c0738115_mads3464_midterm/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moxdroidlabs.c0738115_mads3464_midterm;
+
+/**
+ *
+ * @author moxdroid
+ */
+public class Main {
+    
+}
