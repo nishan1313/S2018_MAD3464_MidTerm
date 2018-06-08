610860b0f9445b7d0ad34037fbec4b1873adfcca
moxDroid <pritam.world@gmail.com>
2018-06-07 20:41:09 -0400

    commit 2

---
+++ C0738115_MADS3464_MidTerm/src/main/java/com/moxdroidlabs/c0738115_mads3464_midterm/BinaryToDecimal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moxdroidlabs.c0738115_mads3464_midterm;
+
+/**
+ *
+ * @author moxdroid
+ */
+public class BinaryToDecimal {
+String BinaryString="1000";
+
+int decimal=Integer.parseInt(BinaryString,2);
+System.out.println(decimal);
+}
